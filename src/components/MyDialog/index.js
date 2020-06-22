c87360ee7eb44983286ad402c04116b8221d75db
ashley <ash.lu@mail.utoronto.ca>
2020-06-22 14:25:12 -0400

    Edited MyDialog styles

--- src/components/MyDialog/index.js
+++ src/components/MyDialog/index.js
@@ -3,6 +3,7 @@ import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
 import ReportIcon from '@material-ui/icons/Report';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 import './styles.css'
 
@@ -13,6 +14,19 @@ class MyDialog extends Component {
             return <ReportIcon/>
         } else if (actionName === 'Remove') {
             return <VisibilityOffIcon/>
+        } else if (actionName === 'Log out') {
+            return <ExitToAppIcon/>
+        }
+    }
+
+    getButtonStyle = () => {
+        const { actionName } = this.props;
+        if (actionName === 'Report') {
+            return 'myDialog__button myDialog__button--report'
+        } else if (actionName === 'Remove') {
+            return 'myDialog__button'
+        } else if (actionName === 'Log out') {
+            return 'myDialog__button'
         }
     }
 
@@ -24,7 +38,7 @@ class MyDialog extends Component {
                     <div className='myDialog__title'>{title}</div>
                     {body !== '' && <div className='myDialog__body'>{body}</div>}
                     <Button 
-                        className='myDialog__report-button'
+                        className={this.getButtonStyle()}
                         variant='outlined'
                         name='report'
                         startIcon={this.getIcon(actionName)}
