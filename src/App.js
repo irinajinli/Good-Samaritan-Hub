5cc11d5af18cb2439b7e81d3e3bc4c477905a57c
Jean Li <jean.li@mail.utoronto.ca>
2020-06-21 20:46:44 -0400

    Modified use of Login component in App.js (added property title). Reformatted code. Removed unused Logo.svg import.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
 
-import logo from "./logo.svg";
 import "./App.css";
 import TopBar from "./components/TopBar";
 import Landing from "./components/Landing";
@@ -13,9 +12,8 @@ import LoggedInWrapper from "./components/LoggedInWrapper";
 import AdminLogin from "./components/AdminLogin";
 import AdminHome from "./components/AdminHome";
 
-import { getUsers, getPosts } from './data/hardCodedData';
-import { reportPost } from './actions/user';
-
+import { getUsers, getPosts } from "./data/hardCodedData";
+import { reportPost } from "./actions/user";
 
 class App extends Component {
   // Global theme
@@ -38,39 +36,56 @@ class App extends Component {
   });
 
   // Initial global state
-  state = {  
+  state = {
     user: getUsers()[0], // Current user that is logged in. TODO: Init to {}/undefined and change this based on which user is logged in
     users: getUsers(),
     posts: getPosts(),
-  }
+  };
 
-  render() { 
+  render() {
     const { user, users, posts } = this.state;
 
-    return (  
+    return (
       <React.Fragment>
         <ThemeProvider theme={this.theme}>
           <BrowserRouter>
             <Switch>
               {/* Shows a different component depending on the exact path in the URL */}
 
-              <Route exact path={["/home", "/profile", "/inbox"]} render={() => <LoggedInWrapper
-                                                                        appComponent={this}
-                                                                        user={user}
-                                                                        posts={posts}
-                                                                        reportPost={reportPost}
-                                                                      />} />
-              <Route exact path="/admin/home" render={() => <AdminHome users={users}/>} />
+              <Route
+                exact
+                path={["/home", "/profile", "/inbox"]}
+                render={() => (
+                  <LoggedInWrapper
+                    appComponent={this}
+                    user={user}
+                    posts={posts}
+                    reportPost={reportPost}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/admin/home"
+                render={() => <AdminHome users={users} />}
+              />
 
               {/* The pages in this fragment share the same top bar */}
               <React.Fragment>
                 <TopBar />
                 <Route exact path="/" render={() => <Landing />} />
-                <Route exact path="/login" render={() => <Login />} />
-                <Route exact path="/registration" render={() => <Registration />} />
+                <Route
+                  exact
+                  path="/login"
+                  render={() => <Login title="User Login" />}
+                />
+                <Route
+                  exact
+                  path="/registration"
+                  render={() => <Registration />}
+                />
                 <Route exact path="/admin" render={() => <AdminLogin />} />
               </React.Fragment>
-
             </Switch>
           </BrowserRouter>
         </ThemeProvider>
@@ -78,5 +93,5 @@ class App extends Component {
     );
   }
 }
- 
+
 export default App;
