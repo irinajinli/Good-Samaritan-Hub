f75cf68474cd92331686ae00ee6db522245b7e9a
Hrithik Shukla <hrithik.shukla@mail.utoronto.ca>
2020-06-24 11:06:12 -0400

    Created basic profile page

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import './styles.css';
+import MailIcon from '@material-ui/icons/Mail';
 
 class Profile extends Component {
-    state = {  }
+    state = { }
     render() { 
-        const { user, displayedUser } = this.props
-        return (  
-            <div>{`${user.firstName} ${user.lastName} is viewing 
-            ${displayedUser.firstName} ${displayedUser.lastName}'s profile`}</div>
-        );
+        const {user, displayedUser} = this.props
+        return (
+        <div className='profile'>
+            <div className='profileContainer'>
+                <div className='imageContainer'>
+                    <img src={require('../../data/userIcon.png')} className='profilePic'/>
+                </div>
+                <div className='textContainer'>
+                    <div className='profileText'>
+                        <h1>Name: {displayedUser.firstName} {displayedUser.lastName}</h1>
+                    </div>
+                    <div className='profileText'>
+                    <h1>Account Type: User </h1>
+                    </div>
+                    <div className = 'profileText'>
+                        <h1>Location: {displayedUser.location} </h1>
+                    </div>
+                </div>
+                <MailIcon/>
+            </div>
+            <div>
+                <h1>
+                    Posts:
+                </h1>
+            </div>
+        </div>
+        )
     }
 }
  
