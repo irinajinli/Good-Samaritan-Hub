5dedb3deb3d7f96d1e732184286cfaa01b2eb990
Jean Li <jean.li@mail.utoronto.ca>
2020-06-30 14:52:38 -0400

    Added credential check to LoginInput.

--- src/components/LoginInput/index.js
+++ src/components/LoginInput/index.js
@@ -4,60 +4,89 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { Box } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import "./styles.css";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: "25ch",
-    },
-  },
-  centre: {
-    position: "absolute",
-    left: "50%",
-    top: "50%",
-    transform: "translate(-50%, -50%)",
-  },
-  marginBottom: {
-    marginBottom: "20px",
-  },
-  loginButton: {
-    marginTop: "20px",
-  },
-  noLeftMargin: {
-    marginLeft: "0px",
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     "& > *": {
+//       margin: theme.spacing(1),
+//       width: "25ch",
+//     },
+//   },
+//   centre: {
+//     position: "absolute",
+//     left: "50%",
+//     top: "50%",
+//     transform: "translate(-50%, -50%)",
+//   },
+//   marginBottom: {
+//     marginBottom: "20px",
+//   },
+//   loginButton: {
+//     marginTop: "20px",
+//   },
+//   noLeftMargin: {
+//     marginLeft: "0px",
+//   },
+// }));
 
-export default function LoginInput() {
-  const classes = useStyles();
+export default class LoginInput extends React.Component {
+  handleOnChange = (event) => {
+    console.log(event.target.value);
 
-  return (
-    <form className={classes.root} noValidate autoComplete="off">
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  goToHome = () => {
+    window.location.href = "/home";
+  };
+
+  checkCredentials = () => {
+    if (this.state.username === "user" && this.state.password === "user") {
+      this.goToHome();
+    } else console.log("credentials incorrect");
+  };
+
+  render() {
+    return (
+      // <form className={classes.root} noValidate autoComplete="off">
       <Box>
         <div>
           <TextField
-            className={classes.marginBottom}
+            className="marginBottom"
             label="Username"
+            name="username"
             variant="outlined"
+            onChange={this.handleOnChange}
           />
         </div>
         <div>
-          <TextField label="Password" variant="outlined" />
+          <TextField
+            label="Password"
+            name="password"
+            variant="outlined"
+            onChange={this.handleOnChange}
+          />
           <Link className="link" to={"/registration"}>
             Need to register?
           </Link>
         </div>
         <div>
           <Button
-            className={classes.loginButton}
             variant="contained"
             color="primary"
+            onClick={() => this.checkCredentials()}
           >
             LOG IN
           </Button>
         </div>
       </Box>
-    </form>
-  );
+      // </form>;
+    );
+  }
 }
