89b01a2dad3d4c91e3636302086c776922ffb138
Jean Li <jean.li@mail.utoronto.ca>
2020-07-03 14:41:13 -0400

    Changed Login to use styles.css instead of MUI's makeStyles

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,42 +1,11 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import { Box } from "@material-ui/core";
-import { Link } from "react-router-dom";
-import Header from "../Header";
 import LoginInput from "../LoginInput";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: "25ch",
-    },
-  },
-  centre: {
-    position: "absolute",
-    left: "50%",
-    top: "50%",
-    transform: "translate(-50%, -50%)",
-  },
-  marginBottom: {
-    marginBottom: "20px",
-  },
-  loginButton: {
-    marginTop: "20px",
-  },
-  noLeftMargin: {
-    marginLeft: "0px",
-  },
-}));
+import "./styles.css";
 
 export default function Login() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.centre}>
-      <Header title="Login" />
+    <div className="centre">
+      <div className="title">Login</div>
       <LoginInput />
     </div>
   );
--- src/components/LoginInput/index.js
+++ src/components/LoginInput/index.js
@@ -1,34 +1,8 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import { Link } from "react-router-dom";
 import "./styles.css";
 import "../../index.css";
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     "& > *": {
-//       margin: theme.spacing(1),
-//       width: "25ch",
-//     },
-//   },
-//   centre: {
-//     position: "absolute",
-//     left: "50%",
-//     top: "50%",
-//     transform: "translate(-50%, -50%)",
-//   },
-//   marginBottom: {
-//     marginBottom: "20px",
-//   },
-//   loginButton: {
-//     marginTop: "20px",
-//   },
-//   noLeftMargin: {
-//     marginLeft: "0px",
-//   },
-// }));
 
 class LoginInput extends React.Component {
   state = {
