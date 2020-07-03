967a0079cd07c9e18b5b8f777008fa90b13f339f
Hrithik Shukla <hrithik.shukla@mail.utoronto.ca>
2020-07-03 09:52:02 -0400

    Added report message button

--- src/components/Inbox/Messages/Message.js
+++ src/components/Inbox/Messages/Message.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react'
-import './styles.css'
+import ReportIcon from '@material-ui/icons/Report';
+import Modal from '@material-ui/core/Modal';
+import MyDialog from '../../MyDialog';
+import './styles.css';
 
 class Message extends Component {
+
+    state = {dialogOpen: false}
+
+    handleDialogOpen = () => {
+        this.setState({ dialogOpen: true });
+    }
+
+    handleDialogClose = () => {
+        this.setState({ dialogOpen: false });
+    }
+
     render() {
         let classMessageName = "messageContainer"
         if (this.props.username == this.props.messageSender) {
             classMessageName += "RightMessage"
+            var message = 
+                <p>{this.props.message}</p>
         } else {
+            var message = 
+                <>
+                <ReportIcon className="reportButton" onClick={this.handleDialogOpen}/>
+                <Modal open={this.state.dialogOpen}>
+                    <MyDialog 
+                                title='Report this message?'
+                                body=''
+                                actionName='Report'
+                                handleClose={this.handleDialogClose}
+                                handleDoAction={this.handleDialogClose}
+                    />
+                </Modal>
+                <p className="rightMessageText"> {this.props.message} </p>
+                </>
             classMessageName += "LeftMessage"
         }
         return (
             <div className={classMessageName}>
-                <p> {this.props.message} </p>
+              {message}
             </div>
         );
     }
