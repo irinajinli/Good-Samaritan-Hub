7a72e36962229f2541f808ec3ff897ee09d2e900
Jean Li <jean.li@mail.utoronto.ca>
2020-07-03 16:53:25 -0400

    Added state to RegBasicInfo to determine whether all req'd fields are filled.

--- src/components/RegBasicInfo/index.js
+++ src/components/RegBasicInfo/index.js
@@ -3,47 +3,93 @@ import TextField from "@material-ui/core/TextField";
 import "../RegAccountInfo/styles.css";
 
 export default class RegBasicInfo extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    phoneNum: "",
+    postCode: "",
+    email: "",
+    reqsSatisfied: false,
+  };
+
+  handleOnChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+    this.updateReqsSatisfied();
+  };
+
+  updateReqsSatisfied = () => {
+    if (
+      // check if any required fields are empty
+      !this.state.firstName ||
+      !this.state.lastName ||
+      !this.state.phoneNum ||
+      !this.state.postCode ||
+      !this.state.email
+    ) {
+      console.log("something is not filled");
+      this.setState({ reqsSatisfied: false });
+    } else {
+      this.setState({ reqsSatisfied: true });
+      console.log("everything filled");
+    }
+  };
+
   render() {
     return (
       <form noValidate autoComplete="off">
         <div>
           <TextField
             required
+            name="firstName"
             className="marginRight"
             label="First name"
             variant="outlined"
+            onChange={this.handleOnChange}
           />
 
           <TextField
             required
+            name="lastName"
             className="marginLeft"
             label="Last name"
             variant="outlined"
+            onChange={this.handleOnChange}
           />
         </div>
 
         <div>
           <TextField
             required
+            name="phoneNum"
             className="marginRight marginTop"
             label="Phone number"
             variant="outlined"
+            onChange={this.handleOnChange}
           />
 
           <TextField
             required
+            name="postCode"
             className="marginLeft marginTop"
             label="Postal code"
             variant="outlined"
+            onChange={this.handleOnChange}
           />
         </div>
 
         <div>
           <TextField
             required
+            name="email"
             className="long marginTop"
             label="Email address"
             variant="outlined"
+            onChange={this.handleOnChange}
           />
         </div>
       </form>
--- src/components/RegBio/index.js
+++ src/components/RegBio/index.js
@@ -1,45 +1,21 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import "../RegAccountInfo/styles.css";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: "25ch",
-    },
-  },
-  marginRight: {
-    marginRight: "4px",
-  },
-  marginLeft: {
-    marginLeft: "4px",
-  },
-  marginTop: {
-    marginTop: "30px",
-  },
-  long: {
-    width: "431.2px",
-  },
-  // tall: {
-  //   height: "200px",
-  // },
-}));
-
-export default function RegBio() {
-  const classes = useStyles();
-
-  return (
-    <form noValidate autoComplete="off">
-      <div>
-        <TextField
-          multiline
-          rows={6}
-          className={`${classes.long} ${classes.tall}`}
-          label="Bio (optional)"
-          variant="outlined"
-        />
-      </div>
-    </form>
-  );
+export default class RegBio extends React.Component {
+  render() {
+    return (
+      <form noValidate autoComplete="off">
+        <div>
+          <TextField
+            multiline
+            rows={6}
+            className="long tall"
+            label="Bio (optional)"
+            variant="outlined"
+          />
+        </div>
+      </form>
+    );
+  }
 }
