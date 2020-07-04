c7fd535df383969ca360a8384e6d7e8e4c3c33ce
ashley <ashleylu2008@gmail.com>
2020-07-03 20:36:58 -0400

    Added a description of the website on the landing page.

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -20,24 +20,13 @@ class Landing extends Component {
         </div>
 
         <LandingBlurb
-          titleText="Title."
-          mainText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum."
-        />
-        <LandingBlurb
-          titleText="Another title."
-          mainText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum."
+          titleText="Hello there."
+          mainText="Welcome to the Good Samaritan Hub.
+          Our goal is to assist vulnerable groups within our communities, such as the elderly and immunocompromised,
+          who do not have anyone to help them. On our site, we match
+          volunteers with people in need of help—for example, with groceries or pickups.
+          Our site currently only supports locations within the GTA, but we plan to expand
+          our outreach in the near future."
         />
 
         <div className="bottomBar">
